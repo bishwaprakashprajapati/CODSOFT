c062ffe1f93411d063867f68b2cd75ef4456dd43
Bishwa Prakash Prajapati <157591905+bishwaprakashprajapati@users.noreply.github.com>
2024-02-28 22:36:46 +0545

    Add files via upload

---
+++ student_result.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+public class student_result {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        int totalMarks = 0;
+        double averagePercentage;
+
+        System.out.println("Enter marks obtained (out of 100) in each subject:");
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Subject " + i + ": ");
+            int marks = scanner.nextInt();
+            totalMarks += marks;
+        }
+
+        averagePercentage = (double) totalMarks / numSubjects;
+
+        String grade;
+        if (averagePercentage >= 90) {
+            grade = "A+";
+        } else if (averagePercentage >= 80) {
+            grade = "A";
+        } else if (averagePercentage >= 70) {
+            grade = "B+";
+        } else if (averagePercentage >= 60) {
+            grade = "B";
+        } else if (averagePercentage >= 50) {
+            grade = "C";
+        } else {
+            grade = "F";
+        }
+
+        System.out.println("\nResults:");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
